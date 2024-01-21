0bd917d8009b7cda3638e4e3b30bfed95a8ffb2a
NicoPotrich <nicolas_potrich@hotmail.com>
2024-01-21 00:50:45 -0300

    Borre los datos del archivo main

--- main.sql
+++ main.sql
@@ -1,10 +1,1 @@
 
-DELIMITER //
-CREATE PROCEDURE procedure_name(
-	IN param_name TYPE, -- pasar parametros a la función
-	OUT param_name TYPE -- cambiar valor del parametro, reescribir valor
-)
-BEGIN
-	-- SQL code
-END //
-DELIMITER ;
